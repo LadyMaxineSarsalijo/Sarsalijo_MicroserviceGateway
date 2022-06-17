e1140174397429dc002c4a004b4fb751139c2b51
LadyMaxineSarsalijo <iammaxine19@gmail.com>
2022-06-17 21:25:48 +0800

    updated gateway

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -29,6 +29,7 @@ $app->withEloquent();
 
 $app->configure('services');
 $app->configure('auth');
+$app->configure('database');
 
 /*
 |--------------------------------------------------------------------------
